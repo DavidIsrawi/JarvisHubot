3dee1984e585569a12272f4e3881c154649e02d0
David <davidisrawi@gmail.com>
2016-08-30 17:28:19 -0400

    oops

--- scripts/router.js
+++ scripts/router.js
@@ -1,7 +1,7 @@
 // Description
 // A router for Mr. Jarvis
 
-module.exports = dunction(robot) {
+module.exports = function(robot) {
 
   robot.router.post('/hello', function(req, res) {
     var data = (req.body.payload)? JSON.parse(req.body.payload) : req.body;
